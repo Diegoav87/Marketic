b6dd86d8a10ce1320e178340df06b747d1300265
Diegoav87 <diegoabdov@gmail.com>
2020-12-21 19:25:39 -0600

    Modified whitenoise again

--- product_network/settings.py
+++ product_network/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
